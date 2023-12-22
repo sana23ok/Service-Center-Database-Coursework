0a46263a7cb1b77cdfe9aa8a0d8f6edb64819bbd
Oksana Hordych <116382404+sana23ok@users.noreply.github.com>
2023-12-22 23:27:40 +0200

    Exam tables added

--- ServiceCenter_creation.sql
+++ ServiceCenter_creation.sql
@@ -29,24 +29,6 @@
 --	addressID INT FOREIGN KEY REFERENCES Address(addressID)
 --);
 
----- MedCard table
---CREATE TABLE MedCard (
---    medCardID INT IDENTITY(1,1) PRIMARY KEY,
---    bloodType VARCHAR(5) NOT NULL,
---    Rh CHAR(1) NOT NULL,
---    issueDate DATE NOT NULL
---);
-
----- Candidate table with FOREIGN KEY constraint
---CREATE TABLE Candidate (
---    TIN INT PRIMARY KEY,
---    Surname VARCHAR(255) NOT NULL,
---    Firstname VARCHAR(255) NOT NULL,
---    dateOfBirth DATE NOT NULL,
---    phoneNumber VARCHAR(15) NOT NULL,
---    ownerID INT FOREIGN KEY REFERENCES MedCard(medCardID)
---);
-
 CREATE TABLE Position
 (
     positionID INT IDENTITY(1,1) PRIMARY KEY,
@@ -73,5 +55,81 @@ CREATE TABLE Worker
     FOREIGN KEY (centerID) REFERENCES ServiceCenter(centerID),
 	FOREIGN KEY (positionID) REFERENCES Position(positionID)
 );
+
+-- MedCard table
+CREATE TABLE MedCard (
+    medCardID INT IDENTITY(1,1) PRIMARY KEY,
+    bloodType VARCHAR(5) NOT NULL,
+    Rh CHAR(1) NOT NULL,
+    issueDate DATE NOT NULL, 
+    validUntil AS DATEADD(YEAR, 8, issueDate) PERSISTED NOT NULL
+);
+
+
+-- Candidate table with FOREIGN KEY constraint
+CREATE TABLE Candidate (
+    TIN INT PRIMARY KEY,
+    Surname VARCHAR(255) NOT NULL,
+    Firstname VARCHAR(255) NOT NULL,
+    dateOfBirth DATE NOT NULL,
+    phoneNumber VARCHAR(15) NOT NULL,
+    ownerID INT FOREIGN KEY REFERENCES MedCard(medCardID)
+);
+
+-- Exam table
+CREATE TABLE Exam (
+    examID INT IDENTITY(1,1) PRIMARY KEY,
+    dateTime DATETIME NOT NULL,
+    result VARCHAR(50) CHECK (result IN ('positive', 'negative')) NOT NULL, 
+    examinerID INT NOT NULL,
+    FOREIGN KEY (examinerID) REFERENCES Worker(workerID)
+);
+
+-- PracticalExam table
+CREATE TABLE PracticalExam (
+    practicalExamID INT IDENTITY(1,1) PRIMARY KEY,
+    examRoute VARCHAR(255) NOT NULL,
+    examID INT,
+    FOREIGN KEY (examID) REFERENCES Exam(examID)
+);
+
+-- TransportVehicle table
+CREATE TABLE TransportVehicle (
+    registrationPlate VARCHAR(15) PRIMARY KEY,
+    technicalCondition VARCHAR(255) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    brand VARCHAR(50) NOT NULL,
+    transmission VARCHAR(20) NOT NULL,
+    instructorID INT, 
+    FOREIGN KEY (instructorID) REFERENCES Worker(workerID)
+);
+
+
+-- Linking table for the many-to-many relationship
+CREATE TABLE PracticalExam_TransportVehicle (
+    practicalExamID INT NOT NULL,
+    registrationPlate VARCHAR(15) NOT NULL,
+    PRIMARY KEY (practicalExamID, registrationPlate),
+    FOREIGN KEY (practicalExamID) REFERENCES PracticalExam(practicalExamID),
+    FOREIGN KEY (registrationPlate) REFERENCES TransportVehicle(registrationPlate)
+);
+
+-- Trigger to check that examinerID in Exam != instructorID in TransportVehicle
+CREATE TRIGGER CheckExaminerInstructor
+ON TransportVehicle
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        JOIN Exam e ON i.instructorID = e.examinerID
+    )
+    BEGIN
+        RAISEERROR('Examiner ID in Exam must not be equal to Instructor ID in TransportVehicle.', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END;
+
 
 
